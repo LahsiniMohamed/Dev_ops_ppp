b0f27cc685601408be2896deac8368ad7dad96ba
LahsiniMohamed <36932931+LahsiniMohamed@users.noreply.github.com>
2021-05-24 15:38:03 +0100

    added a json data file

--- src/components/mainPage/MainPage.js
+++ src/components/mainPage/MainPage.js
@@ -3,13 +3,8 @@ import './MainPage.scss'
 import background from "./images/background.jpg";
 import MovieSelect from "../movieCard/MovieSelect"
 
-const movie_info = {
-    name : 'Avanger',
-    description : 'After the devastating events of Avengers: Infinity War (2018), the universe is in ruins. With the help of remaining allies, the Avengers assemble once more in order to reverse Thanos  qsdqsdqsd qsd qsd qsdqs dazd qsdz',
-    rating : '5',
-    stars : 'Robert Downey Jr., Chris Evans qdqsdzaaeqsdqsdz azd zdq sdaz sqdqzda dqsdaz qsdaza ...',
-    directors : 'Anthony Russo, Joe Russdqsdq azd qsda z sd qda qsdqzdad qsdzd s qsd qsd qsdqsdqsd qd qsd qds qsdq sdqd qsd qs dq sdso',
-    }
+import dataSet from "../../data/movies.json"
+
 
 const image = 'https://cdn11.bigcommerce.com/s-ydriczk/images/stencil/1280x1280/products/88997/93196/Avengers-Endgame-Final-Style-Poster-buy-original-movie-posters-at-starstills__42370.1563973516.jpg?c=2?imbypass=on'
 
@@ -29,12 +24,27 @@ function MainPage() {
         //here put the code that send the movies to the modele and transfere to the next page
         console.log(`Button clicked, the user has choosed ${movies}`)
     }
-
+    
+    const listMovies = dataSet['movies'].map((movie)=>
+        <MovieSelect removeMovie={removeMovie} addMovie={addMovie} id={movie.id} movie_info={movie} image={image} />
+    )
     useEffect(() => {
         console.log(movies)
     },[movies])
 
     //       <div className="mainPage"  style={{ background: `url(${background}) , black`,backgroundSize: 'cover' }}>
+    /*
+                <MovieSelect removeMovie={removeMovie} addMovie={addMovie} id="1" movie_info={movie_info} image={image} />
+                <MovieSelect removeMovie={removeMovie} addMovie={addMovie} id="2" movie_info={movie_info} image='https://cdn.pastemagazine.com/www/system/images/photo_albums/best-movie-posters-2016/large/moonlight-ver2-xlg.jpg?1384968217' />
+                <MovieSelect removeMovie={removeMovie} addMovie={addMovie} id="3" movie_info={movie_info} image= 'https://cdn.shopify.com/s/files/1/0969/9128/products/1917_-_Sam_Mendes_-_Hollywood_War_Film_Classic_English_Movie_Poster_a12704bd-2b25-4aa7-8c8d-8f40cf467dc7.jpg?v=1582781089'/>
+                <MovieSelect removeMovie={removeMovie} addMovie={addMovie} id="4" movie_info={movie_info} image= 'https://cdn.seat42f.com/wp-content/uploads/2020/07/15192015/Project-Power-Movie-Poster-Jamie-Foxx.jpg'/>
+                <MovieSelect removeMovie={removeMovie} addMovie={addMovie} id="5" movie_info={movie_info} image= 'https://cdn.shopify.com/s/files/1/0969/9128/products/1917_-_Sam_Mendes_-_Hollywood_War_Film_Classic_English_Movie_Poster_a12704bd-2b25-4aa7-8c8d-8f40cf467dc7.jpg?v=1582781089'/>
+                <MovieSelect removeMovie={removeMovie} addMovie={addMovie} id="6" movie_info={movie_info} image='https://cdn.seat42f.com/wp-content/uploads/2020/07/15192015/Project-Power-Movie-Poster-Jamie-Foxx.jpg' />
+                <MovieSelect removeMovie={removeMovie} addMovie={addMovie} id="7" movie_info={movie_info} image={image} />
+                <MovieSelect removeMovie={removeMovie} addMovie={addMovie} id="8" movie_info={movie_info} image='https://cdn.seat42f.com/wp-content/uploads/2020/07/15192015/Project-Power-Movie-Poster-Jamie-Foxx.jpg' />
+                <MovieSelect removeMovie={removeMovie} addMovie={addMovie} id="9" movie_info={movie_info} image='https://cdn.shopify.com/s/files/1/0969/9128/products/1917_-_Sam_Mendes_-_Hollywood_War_Film_Classic_English_Movie_Poster_a12704bd-2b25-4aa7-8c8d-8f40cf467dc7.jpg?v=1582781089' />
+            
+    */
 
     return (
         <div className="mainPage"> 
@@ -46,16 +56,10 @@ function MainPage() {
                 </div>
                 <button className = "mainPage__button" > Search bar here</button>
             </div>
+            
             <div className="mainPage__cards">
-                <MovieSelect removeMovie={removeMovie} addMovie={addMovie} id="1" movie_info={movie_info} image={image} />
-                <MovieSelect removeMovie={removeMovie} addMovie={addMovie} id="2" movie_info={movie_info} image='https://cdn.pastemagazine.com/www/system/images/photo_albums/best-movie-posters-2016/large/moonlight-ver2-xlg.jpg?1384968217' />
-                <MovieSelect removeMovie={removeMovie} addMovie={addMovie} id="3" movie_info={movie_info} image= 'https://cdn.shopify.com/s/files/1/0969/9128/products/1917_-_Sam_Mendes_-_Hollywood_War_Film_Classic_English_Movie_Poster_a12704bd-2b25-4aa7-8c8d-8f40cf467dc7.jpg?v=1582781089'/>
-                <MovieSelect removeMovie={removeMovie} addMovie={addMovie} id="4" movie_info={movie_info} image= 'https://cdn.seat42f.com/wp-content/uploads/2020/07/15192015/Project-Power-Movie-Poster-Jamie-Foxx.jpg'/>
-                <MovieSelect removeMovie={removeMovie} addMovie={addMovie} id="5" movie_info={movie_info} image= 'https://cdn.shopify.com/s/files/1/0969/9128/products/1917_-_Sam_Mendes_-_Hollywood_War_Film_Classic_English_Movie_Poster_a12704bd-2b25-4aa7-8c8d-8f40cf467dc7.jpg?v=1582781089'/>
-                <MovieSelect removeMovie={removeMovie} addMovie={addMovie} id="6" movie_info={movie_info} image='https://cdn.seat42f.com/wp-content/uploads/2020/07/15192015/Project-Power-Movie-Poster-Jamie-Foxx.jpg' />
-                <MovieSelect removeMovie={removeMovie} addMovie={addMovie} id="7" movie_info={movie_info} image={image} />
-                <MovieSelect removeMovie={removeMovie} addMovie={addMovie} id="8" movie_info={movie_info} image='https://cdn.seat42f.com/wp-content/uploads/2020/07/15192015/Project-Power-Movie-Poster-Jamie-Foxx.jpg' />
-                <MovieSelect removeMovie={removeMovie} addMovie={addMovie} id="9" movie_info={movie_info} image='https://cdn.shopify.com/s/files/1/0969/9128/products/1917_-_Sam_Mendes_-_Hollywood_War_Film_Classic_English_Movie_Poster_a12704bd-2b25-4aa7-8c8d-8f40cf467dc7.jpg?v=1582781089' />
+                {listMovies}
+                
             </div>
         </div>
     )
--- src/components/movieCard/MovieBadge.js
+++ src/components/movieCard/MovieBadge.js
@@ -3,14 +3,14 @@ import "./MovieBadge.scss"
 import StarIcon from '@material-ui/icons/Star';
 
 function MovieBadge({movie_info, ...props}) {
-    const {name , description , rating , stars , directors} = movie_info
+    const {title , description , rating , cast , director} = movie_info
     return (
         <div className = "movieBadge">
-            <h3>{name}</h3>
+            <h3>{title}</h3>
             <p className = "movieBadge_desc">{description}</p>
             <div className="movieBadge__staff">
-                <p className="movieBadge_stars">Stars : <span>{stars}</span></p>
-                <p className="movieBadge_directors">Description <span>{directors}</span></p>
+                <p className="movieBadge_stars">Cast : <span>{cast}</span></p>
+                <p className="movieBadge_directors">Director : <span>{director}</span></p>
             </div>
             <div className="movieBadge__rating">
                 <h4>{rating}<span>/10</span></h4>
